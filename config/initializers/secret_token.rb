e3bb95ddf481d5e20b98e0ae3f73a2168a3c501f
Nick Wales <nick@nickwales.co.uk>
2011-06-30 16:47:20 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Squashleaguemanager::Application.config.secret_token = 'dd57d8564375569ba487baf6831b744e5476baeff07601401b537243150e483433eb9a7775a10bc41776439dc7153a2af4664aee1486055860969f864bebcd95'
